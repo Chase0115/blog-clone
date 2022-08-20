3b677e91d3e91a7ffc4b7461b6976d6e15af4522
Chase <devchase0115@gmail.com>
2022-08-20 19:59:10 +1000

    UI - single, singlePost

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Topbar from './components/topbar/Topbar';
 import Home from './pages/home/Home'
+import Single from './pages/single/Single';
 
 
 function App() {
   return (
     <div>
       <Topbar />
-      <Home />
+      <Single />
     </div>
   );
 }
--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -3,7 +3,36 @@ import './singlePost.css'
 const SinglePost = () => {
   return (
     <div className="singlePost">
-      singlePost
+      <div className="singlePostWrapper">
+        <img src="/images/react.jpg" alt="singlePostImage"/>
+        <div className="singlePostTitlebar">
+          <h1 className="singlePostTitle">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio exercitationem quia 
+          </h1>
+          <div className="singlePostButtons">
+            <button className="titleButton edit">
+              <i className="fa-solid fa-pen-to-square"></i>
+            </button>
+            <button className="titleButton remove">
+              <i className="fa-solid fa-trash-can"></i>
+            </button>
+          </div>
+        </div>
+        <div className="singlePostBody">
+          <div className="bodyInfo">
+            <p className="bodyDate"> Updated: 1 Hour ago</p>
+            <p className="bodyAuthor"> Author: DevChase</p>
+          </div>
+          <article className="bodyDesc">
+            
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione sit, sequi vel fugit atque animi dolore, a expedita facere recusandae iusto numquam officiis eligendi dolor magnam. Inventore laboriosam iste reprehenderit!
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione sit, sequi vel fugit atque animi dolore, a expedita facere recusandae iusto numquam officiis eligendi dolor magnam. Inventore laboriosam iste reprehenderit!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione sit, sequi vel fugit atque animi dolore, a expedita facere recusandae iusto numquam officiis eligendi dolor magnam. Inventore laboriosam iste reprehenderit!
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione sit, sequi vel fugit atque animi dolore, a expedita facere recusandae iusto numquam officiis eligendi dolor magnam. Inventore laboriosam iste reprehenderit!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione sit, sequi vel fugit atque animi dolore, a expedita facere recusandae iusto numquam officiis eligendi dolor magnam. Inventore laboriosam iste reprehenderit!
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione sit, sequi vel fugit atque animi dolore, a expedita facere recusandae iusto numquam officiis eligendi dolor magnam. Inventore laboriosam iste reprehenderit!
+          </article>
+        </div>
+
+      </div>
     </div>
   )
 };
