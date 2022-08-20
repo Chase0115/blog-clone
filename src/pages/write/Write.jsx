8197dcf360caf958fb8099315edef55195c6e7e4
Chase <devchase0115@gmail.com>
2022-08-20 20:32:59 +1000

    UI - write

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import './write.css'
 
 const Write = () => {
   return(
-    <div></div>
+    <div className="write">
+      <img 
+        className="writeImg"
+        src="/images/writeImage.jpeg" 
+        alt="writeImage" />
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+          <i className="writeIcon fa-solid fa-plus"></i>
+          </label>
+          <input 
+            type="file" 
+            style={{display: "none"}} id="fileInput"/>
+          <input 
+            type="text" 
+            placeholder="Title" 
+            className="writeInput"
+            autoFocus={true}
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea 
+            placeholder="Tell me your story ..."
+            type="text"
+            className="writeInput writeText">
+            </textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
+      </form>
+    </div>
   )
 };
 
